9be2c351a64d9a8b9e0c4f27d451b8815a23bbec
mjuhas537 <mjuhas537@gmail.com>
2024-08-19 23:19:56 +0200

    clear code

--- src/app.ts
+++ src/app.ts
@@ -11,20 +11,16 @@ import {
   updateTask,
 } from "./database/utils";
 
-var bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const passport = require("passport");
 const initializePassport = require("./passport-config");
 const flash = require("express-flash");
 const session = require("express-session");
-app.use(express.urlencoded({ extended: false }));
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 app.use(express.static(path.join(__dirname, "./views")));
